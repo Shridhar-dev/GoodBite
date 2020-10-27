bda839364463c4e4f74deeeaeb9a65fb31b9fa06
Shridhar-dev <shridharkamat10@gmail.com>
2020-10-27 13:25:17 +0530

    first one

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
+import Trusted from './Trusted';
+import Footer from './Footer';
+import Charity from './Charity';
+import Delivery from './Delivery';
+import CardSection from './CardSection';
+import Info from './Info';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Trusted />
+      <Info />
+      <CardSection />
+      <Delivery />
+      <Charity />
+      <Footer />
     </div>
   );
 }
